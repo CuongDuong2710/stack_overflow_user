c8c6823c171bb75f0bf1eb469f600212a83c832d
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-03-20 21:42:21 +0700

    set up bottom to navigate fragment

---
+++ app/src/main/java/cuongduong/developer/android/stackoverflow/ui/bookmark/BookmarkViewModel.kt
@@ -0,0 +1,7 @@
+package cuongduong.developer.android.stackoverflow.ui.bookmark
+
+import androidx.lifecycle.ViewModel;
+
+class BookmarkViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/cuongduong/developer/android/stackoverflow/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package cuongduong.developer.android.stackoverflow.ui.home
+
+import androidx.lifecycle.ViewModel;
+
+class HomeViewModel : ViewModel() {
+
+}
