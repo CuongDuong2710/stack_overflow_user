88495ee6ca51ffb793793992967c216fc19d0ca6
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-03-24 00:21:15 +0700

    Glide modules

---
+++ app/src/main/java/cuongduong/developer/android/stackoverflow/internal/glide/StackExchangeGlideModule.kt
@@ -0,0 +1,7 @@
+package cuongduong.developer.android.stackoverflow.internal.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class StackExchangeGlideModule : AppGlideModule()
