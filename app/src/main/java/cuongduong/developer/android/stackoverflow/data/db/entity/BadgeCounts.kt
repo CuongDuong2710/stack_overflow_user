9cba6869b61c52be040a7a50e64bd12004c2da9d
cuong.duong <cuong.duong@titancorpvn.com>
2019-03-26 12:35:58 +0700

    remove redundant field

--- app/src/main/java/cuongduong/developer/android/stackoverflow/data/db/entity/BadgeCounts.kt
+++
@@ -1,7 +0,0 @@
-package cuongduong.developer.android.stackoverflow.data.db.entity
-
-data class BadgeCounts(
-    val bronze: Int,
-    val silver: Int,
-    val gold: Int
-)
--- app/src/main/java/cuongduong/developer/android/stackoverflow/data/db/entity/Item.kt
+++ app/src/main/java/cuongduong/developer/android/stackoverflow/data/db/entity/Item.kt
@@ -8,42 +8,14 @@ import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "item")
 data class Item(
-    @Embedded(prefix = "badge_counts_")
-    @SerializedName("badge_counts")
-    val badgeCounts: BadgeCounts,
-    @SerializedName("account_id")
-    val accountId: Int,
-    @SerializedName("is_employee")
-    val isEmployee: Boolean,
-    @SerializedName("last_modified_date")
-    val lastModifiedDate: Int,
     @SerializedName("last_access_date")
     val lastAccessDate: Long,
-    @SerializedName("reputation_change_year")
-    val reputationChangeYear: Int,
-    @SerializedName("reputation_change_quarter")
-    val reputationChangeQuarter: Int,
-    @SerializedName("reputation_change_month")
-    val reputationChangeMonth: Int,
-    @SerializedName("reputation_change_week")
-    val reputationChangeWeek: Int,
-    @SerializedName("reputation_change_day")
-    val reputationChangeDay: Int,
     val reputation: Int,
-    @SerializedName("creation_date")
-    val creationDate: Int,
-    @SerializedName("user_type")
-    val userType: String,
     @SerializedName("user_id")
     @PrimaryKey
     val userId: Int,
-    @SerializedName("accept_rate")
-    val acceptRate: Int,
     @Nullable
     val location: String? = null,
-    @SerializedName("website_url")
-    val websiteUrl: String,
-    val link: String,
     @SerializedName("profile_image")
     val profileImage: String,
     @SerializedName("display_name")
